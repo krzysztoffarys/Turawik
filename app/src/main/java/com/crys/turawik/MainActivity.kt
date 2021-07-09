aa6d629e80e2a96f9901644d4efdafa997061566
Crys <krzysztoffarys00@gmail.com>
2021-07-09 06:58:58 +0200

    Bottom nav menu

--- app/src/main/java/com/crys/turawik/MainActivity.kt
+++ app/src/main/java/com/crys/turawik/MainActivity.kt
@@ -2,11 +2,17 @@ package com.crys.turawik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crys.turawik.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Turawik)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
